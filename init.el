782e621e15fbfba57bc971794b6f5d38c3865591
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-15 02:20:55 -0600

    multiple-cursors: layer added

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
      load-local-layers
      smartparens-local
      org-local
+     multiple-cursors
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
---
+++ multiple-cursors/packages.el
@@ -0,0 +1,63 @@
+;;; packages.el --- multiple-cursors Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(setq multiple-cursors-packages '(multiple-cursors))
+
+;; List of packages to exclude.
+(setq multiple-cursors-excluded-packages '())
+
+(defun hb/configure-mc-isearch ()
+  (defvar jc/mc-search--last-term nil)
+  (defun jc/mc-search (search-command)
+    ;; Read new search term when not repeated command or applying to fake cursors
+    (when (and (not mc--executing-command-for-fake-cursor)
+               (not (eq last-command 'jc/mc-search-forward))
+               (not (eq last-command 'jc/mc-search-backward)))
+      (setq jc/mc-search--last-term (read-from-minibuffer "Search: ")))
+    (funcall search-command jc/mc-search--last-term))
+  (defun jc/mc-search-forward ()
+    "Simplified version of forward search that supports multiple cursors"
+    (interactive)
+    (jc/mc-search 'search-forward))
+  (defun jc/mc-search-backward ()
+    "Simplified version of backward search that supports multiple cursors"
+    (interactive)
+    (jc/mc-search 'search-backward)))
+
+(defun hb/configure-mc-bindings ()
+  (bind-key "C-S-c C-S-c" 'mc/edit-lines)
+  (bind-key "C-S-SPC" 'set-rectangular-region-anchor)
+  (bind-key "C->" 'mc/mark-next-like-this)
+  (bind-key "C-<" 'mc/mark-previous-like-this)
+  (bind-key "C-c C-<" 'mc/mark-all-like-this)
+  (bind-key "C-c C->" 'mc/mark-all-like-this-dwim)
+  (bind-key "C-c ~" 'mc/insert-numbers)
+  (bind-key "M-~" 'mc/sort-regions)
+  (bind-key "C-~" 'mc/reverse-regions)
+  (bind-key "C-S-c C-e" 'mc/edit-ends-of-lines)
+  (bind-key "C-S-c C-a" 'mc/edit-beginnings-of-lines))
+
+(defun hb/configure-mc-fixes ()
+  (bind-key "M-SPC" 'just-one-space mc/keymap))
+
+(defun hb/configure-mc ()
+  (hb/configure-mc-isearch)
+  (hb/configure-mc-bindings)
+  (hb/configure-mc-fixes))
+
+(defun multiple-cursors/init-multiple-cursors ()
+  (use-package multiple-cursors
+    :config (hb/configure-mc))
+  (bind-key "C-s" 'jc/mc-search-forward mc/keymap)
+  (bind-key "C-r" 'jc/mc-search-backward mc/keymap))
