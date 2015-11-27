d69149c2d10b2777638b58d245b4304ea7de596d
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-26 21:23:20 -0600

    forgot to remove

--- init.el
+++ init.el
@@ -46,7 +46,6 @@
      lisp-local
      hb-custom
      hb-c++
-     hb-swift
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
