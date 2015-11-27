bfde360cb36ef7d20bc6ff40cf71d18ae968a9e6
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-26 20:10:12 -0600

    init: use hb-{custom,c++} layers

--- init.el
+++ init.el
@@ -40,11 +40,12 @@
      html
      syntax-checking
      version-control
-     load-local-layers
      smartparens-local
      org-local
      multiple-cursors
      lisp-local
+     hb-custom
+     hb-c++
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
