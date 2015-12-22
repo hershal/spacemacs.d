6b929aef7935391598275211ffb54b17bbab6070
Hershal Bhave <hershal.bhave@gmail.com>
2015-12-22 15:28:13 -0600

    init: enabled hb-swift

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
      lisp-local
      hb-custom
      hb-c++
-     )
+     hb-swift)
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
