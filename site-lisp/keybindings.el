4d9fdfb3c29325831720470b3f2c37ab56af764f
Hershal Bhave <hershal.bhave@gmail.com>
2016-05-17 20:12:28 -0500

    site-lisp/keybindings: forgot to remove this guy

--- site-lisp/keybindings.el
+++ site-lisp/keybindings.el
@@ -1,8 +1,6 @@
 (bind-key "C-h C-F" 'find-function)
 (bind-key "M-x" 'execute-extended-command)
 
-(bind-key "C-h" 'help)
-
 (bind-key "C-h" 'helm-find-files-up-one-level helm-find-files-map)
 (bind-key "C-l" 'helm-execute-persistent-action helm-find-files-map)
 
