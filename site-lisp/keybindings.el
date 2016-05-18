176ed3f2bcdb260e803e0ff4a869e7aec2f99484
Hershal Bhave <hershal.bhave@gmail.com>
2016-05-17 21:18:51 -0500

    site-lisp/keybindings: helm is annoying

--- site-lisp/keybindings.el
+++ site-lisp/keybindings.el
@@ -1,6 +1,7 @@
 (bind-key "C-h C-F" 'find-function)
 (bind-key "M-x" 'execute-extended-command)
 
+(require 'helm-files)
 (bind-key "C-h" 'helm-find-files-up-one-level helm-find-files-map)
 (bind-key "C-l" 'helm-execute-persistent-action helm-find-files-map)
 
