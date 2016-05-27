635936d264e50c78f99ee1d229dbbc00716ab41d
Hershal Bhave <hershal.bhave@gmail.com>
2016-05-27 17:57:29 -0500

    js2: remove broken tab command

--- site-lisp/hb-init.el
+++ site-lisp/hb-init.el
@@ -1,6 +1,7 @@
 (defun hb/init ()
   (require 'node)
-  (require 'keybindings))
+  (require 'keybindings)
+  (require 'js2-mode-hooks))
 
 (hb/init)
 (provide 'hb-init)
---
+++ site-lisp/js2-mode-hooks.el
@@ -0,0 +1,6 @@
+(defun js2-hooks/remove-broken-tab-complete ()
+  (setq tab-always-indent t))
+
+(add-hook 'js2-mode-hook 'js2-hooks/remove-broken-tab-complete)
+
+(provide 'js2-mode-hooks)
