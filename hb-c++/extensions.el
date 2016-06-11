e4508369859495f468ff200cae72248d9706a12f
Hershal Bhave <hershal.bhave@gmail.com>
2016-06-11 17:27:28 -0500

    hb-c++: set up flycheck for c++11

--- hb-c++/extensions.el
+++ hb-c++/extensions.el
@@ -5,6 +5,10 @@
   (add-hook 'c-mode-common-hook 'hb-c++/common-hook))
 
 (defun hb-c++/common-hook ()
+  (set (make-local-variable 'compile-command)
+       (concat "g++ -std=c++11 -Wall " buffer-file-name " && ./a.out"))
+  (setq company-clang-arguments '("-std=c++11"))
+  (setq flycheck-clang-language-standard "c++11")
   (setq comment-start "/*"
         comment-end "*/"
         c-basic-offset 4)
