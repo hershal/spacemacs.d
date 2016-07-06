dafcd42908168e37af1a7228a4bf8defd7101e6b
Hershal Bhave <hershal.bhave@gmail.com>
2016-07-05 22:33:46 -0500

    init-new: added yasnippet

--- init.el
+++ init.el
@@ -87,6 +87,7 @@ values."
      hb-org
      hb-multiple-cursors
      hb-diminish
+     hb-yasnippet
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
