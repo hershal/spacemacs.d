5b898dc89faac5122e19bae5043b8180184321da
Hershal Bhave <hershal.bhave@gmail.com>
2016-09-11 01:13:30 -0500

    web: set web-related offsets to 2

--- init.el
+++ init.el
@@ -91,6 +91,7 @@ values."
      hb-yasnippet
      hb-javascript
      hb-expand-region
+     hb-web
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
