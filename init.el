7538c15a9d238ac80e5e9df34324f5485856f3ea
Hershal Bhave <hershal.bhave@gmail.com>
2016-10-09 19:11:02 -0500

    init: reenable layers

--- init.el
+++ init.el
@@ -100,16 +100,16 @@ values."
      version-control
 
      ;; hb layers
-     ;; hb-smartparens
-     ;; hb-cmake
-     ;; hb-lisp
-     ;; hb-org
-     ;; hb-multiple-cursors
-     ;; hb-diminish
-     ;; hb-yasnippet
-     ;; hb-javascript
-     ;; hb-expand-region
-     ;; hb-web
+     hb-smartparens
+     hb-cmake
+     hb-lisp
+     hb-org
+     hb-multiple-cursors
+     hb-diminish
+     hb-yasnippet
+     hb-javascript
+     hb-expand-region
+     hb-web
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
