855231b70a2c6005f5dd3956ad46c1b42012e8f7
Hershal Bhave <hershal.bhave@gmail.com>
2017-10-26 15:02:29 -0700

    Update org-headline-done face and headline fontification

--- init.el
+++ init.el
@@ -373,3 +373,10 @@ you should place your code here."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:foreground "red" :inherit aw-leading-char-face :height 3.0))))
+ '(org-headline-done ((t (:foreground "RosyBrown" :strike-through t)))))
